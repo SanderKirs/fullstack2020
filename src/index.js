111751086930e6b953d98a158b82df00da70ed5a
Sander Kirs <sander.kirs@khk.ee>
2021-01-11 10:55:54 +0200

    Exercise 1.2

--- src/index.js
+++ src/index.js
@@ -11,6 +11,40 @@ const Header = () => {
   )
 }
 
+const Part1 = () => {
+  const part1 = 'Fundamentals of React'
+  const exercises1 = 10
+  return(
+    <div>
+      <p>
+        {part1} {exercises1}
+      </p>
+    </div>
+  )
+}
+const Part2 = () => {
+  const part2 = 'Using props to pass data'
+  const exercises2 = 7
+  return(
+    <div>
+      <p>
+        {part2} {exercises2}
+      </p>
+    </div>
+  )
+}
+const Part3 = () => {
+  const part3 = 'State of a component'
+  const exercises3 = 14
+  return(
+    <div>
+      <p>
+        {part3} {exercises3}
+      </p>
+    </div>
+  )
+}
+
 const Content = () => {
   const part1 = 'Fundamentals of React'
   const exercises1 = 10
@@ -20,15 +54,9 @@ const Content = () => {
   const exercises3 = 14
   return(
     <div>
-      <p>
-        {part1} {exercises1}
-      </p>
-      <p>
-        {part2} {exercises2}
-      </p>
-      <p>
-        {part3} {exercises3}
-      </p>
+      <Part1 part1={part1} exercises1={exercises1} />
+      <Part2 part2={part2} exercises2={exercises2}/>
+      <Part3 part3={part3} exercises3={exercises3}/>
     </div>
   )
 }
